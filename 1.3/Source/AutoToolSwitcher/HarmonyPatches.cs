50d2a62ac191aae76eed77b7c470fdb36d77f7aa
Taranchuk <nfhfyxer@gmail.com>
2021-08-23 22:58:09 +0300

    update

--- 1.3/Source/AutoToolSwitcher/HarmonyPatches.cs
+++ 1.3/Source/AutoToolSwitcher/HarmonyPatches.cs
@@ -245,108 +245,112 @@ namespace AutoToolSwitcher
         private static Dictionary<Job, ThingWithComps> cachedToolsByJobs = new Dictionary<Job, ThingWithComps>();
         private static void AddEquipToolToilsPostfix(ref IEnumerable<Toil> __result, JobDriver __instance)
         {
-            var pawn = __instance.pawn;
-            if (!ignoredJobs.Contains(__instance.job.def) && (pawn?.RaceProps?.Humanlike ?? false))
+            try
             {
-                var list = __result.ToList();
-                var skill = ToolSearchUtility.GetActiveSkill(pawn.CurJob, list);
-                if (pawn.Map != null)
+                var pawn = __instance.pawn;
+                if (!ignoredJobs.Contains(__instance.job.def) && (pawn?.RaceProps?.Humanlike ?? false))
                 {
-                    ToolAction toolAction;
-                    var tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
-                    cachedToolsByJobs[pawn.CurJob] = tool; // we do that so we retrieve this tool later rather than finding it again
-                    if (tool != null)
+                    var list = __result.ToList();
+                    var skill = ToolSearchUtility.GetActiveSkill(pawn.CurJob, list);
+                    if (pawn.Map != null)
                     {
-                        Toil goToTool = new Toil();
-                        goToTool.initAction = delegate
+                        ToolAction toolAction;
+                        var tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
+                        cachedToolsByJobs[pawn.CurJob] = tool; // we do that so we retrieve this tool later rather than finding it again
+                        if (tool != null)
                         {
-                            if (!pawn.Reserve(tool, pawn.CurJob, 1, 1))
+                            Toil goToTool = new Toil();
+                            goToTool.initAction = delegate
                             {
-                                pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
-                            }
-                            else
+                                if (!pawn.Reserve(tool, pawn.CurJob, 1, 1))
+                                {
+                                    pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
+                                }
+                                else
+                                {
+                                    pawn.pather.StartPath(tool, PathEndMode.OnCell);
+                                }
+                            };
+                            goToTool.defaultCompleteMode = ToilCompleteMode.PatherArrival;
+                            goToTool.AddEndCondition(delegate
                             {
-                                pawn.pather.StartPath(tool, PathEndMode.OnCell);
+                                return (tool != null && tool.Spawned && tool.Map == pawn.Map) ? JobCondition.Ongoing : JobCondition.Incompletable;
+                            });
+                            Toil equipTool = new Toil();
+                            equipTool.initAction = delegate
+                            {
+                                EquipTool(pawn, tool);
+                            };
+                            switch (toolAction)
+                            {
+                                case ToolAction.GoAndEquipTool:
+                                    list.Insert(0, goToTool);
+                                    list.Insert(1, equipTool);
+                                    break;
+                                case ToolAction.EquipFromInventory:
+                                    list.Insert(0, equipTool);
+                                    break;
                             }
-                        };
-                        goToTool.defaultCompleteMode = ToilCompleteMode.PatherArrival;
-                        goToTool.AddEndCondition(delegate
-                        {
-                            return (tool != null && tool.Spawned && tool.Map == pawn.Map) ? JobCondition.Ongoing : JobCondition.Incompletable;
-                        });
-                        Toil equipTool = new Toil();
-                        equipTool.initAction = delegate
-                        {
-                            EquipTool(pawn, tool);
-                        };
-                        switch (toolAction)
-                        {
-                            case ToolAction.GoAndEquipTool:
-                                list.Insert(0, goToTool);
-                                list.Insert(1, equipTool);
-                                break;
-                            case ToolAction.EquipFromInventory:
-                                list.Insert(0, equipTool);
-                                break;
                         }
                     }
+                    //else if (Scribe.mode == LoadSaveMode.PostLoadInit && Current.ProgramState == ProgramState.MapInitializing)
+                    //// if a save was reloaded, pawn map will be null when setting up jobdriver. in this case we insert tool finder inside actions.
+                    //// And we can't use job target fields because they might be filled
+                    //{
+                    //    //Toil goToTool = new Toil();
+                    //    //goToTool.initAction = delegate
+                    //    //{
+                    //    //    ToolAction toolAction;
+                    //    //    var tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
+                    //    //    cachedToolsByJobs[pawn.CurJob] = tool; // we do that so we retrieve this tool later rather than finding it again
+                    //    //    if (tool != null)
+                    //    //    {
+                    //    //        if (toolAction == ToolAction.GoAndEquipTool)
+                    //    //        {
+                    //    //            if (!pawn.Reserve(tool, pawn.CurJob, 1, 1))
+                    //    //            {
+                    //    //                pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
+                    //    //            }
+                    //    //            else
+                    //    //            {
+                    //    //                pawn.pather.StartPath(tool, PathEndMode.OnCell);
+                    //    //            }
+                    //    //        }
+                    //    //    }
+                    //    //};
+                    //    //goToTool.defaultCompleteMode = ToilCompleteMode.Instant;
+                    //    //goToTool.AddEndCondition(delegate
+                    //    //{
+                    //    //    if (!cachedToolsByJobs.TryGetValue(pawn.CurJob, out var tool))
+                    //    //    {
+                    //    //        ToolAction toolAction;
+                    //    //        tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
+                    //    //        cachedToolsByJobs[pawn.CurJob] = tool;
+                    //    //    }
+                    //    //    return tool != null && tool.Spawned && tool.Map == pawn.Map ? JobCondition.Ongoing : JobCondition.Incompletable;
+                    //    //});
+                    //    //Toil equipTool = new Toil();
+                    //    //equipTool.initAction = delegate
+                    //    //{
+                    //    //    if (!cachedToolsByJobs.TryGetValue(pawn.CurJob, out var tool))
+                    //    //    {
+                    //    //        ToolAction toolAction;
+                    //    //        tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
+                    //    //        cachedToolsByJobs[pawn.CurJob] = tool;
+                    //    //    }
+                    //    //    if (tool != null)
+                    //    //    {
+                    //    //        EquipTool(pawn, tool);
+                    //    //    }
+                    //    //};
+                    //    //equipTool.defaultCompleteMode = ToilCompleteMode.Instant;
+                    //    list.Insert(0, new Toil());
+                    //    list.Insert(1, new Toil());
+                    //}
+                    __result = list;
                 }
-                //else if (Scribe.mode == LoadSaveMode.PostLoadInit && Current.ProgramState == ProgramState.MapInitializing)
-                //// if a save was reloaded, pawn map will be null when setting up jobdriver. in this case we insert tool finder inside actions.
-                //// And we can't use job target fields because they might be filled
-                //{
-                //    //Toil goToTool = new Toil();
-                //    //goToTool.initAction = delegate
-                //    //{
-                //    //    ToolAction toolAction;
-                //    //    var tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
-                //    //    cachedToolsByJobs[pawn.CurJob] = tool; // we do that so we retrieve this tool later rather than finding it again
-                //    //    if (tool != null)
-                //    //    {
-                //    //        if (toolAction == ToolAction.GoAndEquipTool)
-                //    //        {
-                //    //            if (!pawn.Reserve(tool, pawn.CurJob, 1, 1))
-                //    //            {
-                //    //                pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
-                //    //            }
-                //    //            else
-                //    //            {
-                //    //                pawn.pather.StartPath(tool, PathEndMode.OnCell);
-                //    //            }
-                //    //        }
-                //    //    }
-                //    //};
-                //    //goToTool.defaultCompleteMode = ToilCompleteMode.Instant;
-                //    //goToTool.AddEndCondition(delegate
-                //    //{
-                //    //    if (!cachedToolsByJobs.TryGetValue(pawn.CurJob, out var tool))
-                //    //    {
-                //    //        ToolAction toolAction;
-                //    //        tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
-                //    //        cachedToolsByJobs[pawn.CurJob] = tool;
-                //    //    }
-                //    //    return tool != null && tool.Spawned && tool.Map == pawn.Map ? JobCondition.Ongoing : JobCondition.Incompletable;
-                //    //});
-                //    //Toil equipTool = new Toil();
-                //    //equipTool.initAction = delegate
-                //    //{
-                //    //    if (!cachedToolsByJobs.TryGetValue(pawn.CurJob, out var tool))
-                //    //    {
-                //    //        ToolAction toolAction;
-                //    //        tool = skill != null ? ToolSearchUtility.FindToolFor(pawn, skill, out toolAction) : ToolSearchUtility.FindToolFor(pawn, pawn.CurJob, out toolAction);
-                //    //        cachedToolsByJobs[pawn.CurJob] = tool;
-                //    //    }
-                //    //    if (tool != null)
-                //    //    {
-                //    //        EquipTool(pawn, tool);
-                //    //    }
-                //    //};
-                //    //equipTool.defaultCompleteMode = ToilCompleteMode.Instant;
-                //    list.Insert(0, new Toil());
-                //    list.Insert(1, new Toil());
-                //}
-                __result = list;
             }
+            catch { }
         }
 
         private static void EquipTool(Pawn pawn, ThingWithComps tool)
